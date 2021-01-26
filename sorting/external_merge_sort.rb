f2275ed218bdb15f4ec5de69850373a557bdf02e
anvox <thien.an.vo.nguyen@gmail.com>
2021-01-26 13:12:40 +0700

    [WIP] External merge sort

--- sorting/external_merge_sort.rb
+++ sorting/external_merge_sort.rb
@@ -1,5 +1,21 @@
-def process(arr)
+def split(file_path)
+  []
 end
 
-arr = File.read('./tests/1000.txt').split(" ").map(&:to_i)
-puts process(arr)
+def sort(file)
+  ""
+end
+
+def merge(files)
+  ""
+end
+
+def process(file_path)
+  parts = split(file_path)
+  sorted_chunk_files = parts.each.map do |chunk_file|
+    sort(chunk_file)
+  end
+  merge(sorted_chunk_files)
+end
+
+puts process('./tests/1000.txt')
