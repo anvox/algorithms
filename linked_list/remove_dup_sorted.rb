5479e2d1ff3c6b242bdf291f254dc0a2eb929af7
anvox <thien.an.vo.nguyen@gmail.com>
2021-02-06 10:46:16 +0700

    Remove duplicated sorted linked list

---
+++ linked_list/remove_dup_sorted.rb
@@ -0,0 +1,42 @@
+# Remove duplicated value in sorted linked list
+
+require './node'
+
+def process(root_node)
+end
+
+root = nil
+curr = nil
+File.read('./tests/4.txt').split(" ").each do |i|
+  node = Node.new(i.to_i)
+  if root.nil?
+    root = node
+  end
+  if curr.nil?
+    curr = node
+  else
+    curr.next_node = node
+    curr = node
+  end
+end
+
+puts "Before: #{root}"
+
+cursor = root
+while cursor != nil
+  if cursor.next_node && cursor.value == cursor.next_node.value
+    c2 = cursor.next_node
+    while c2 && c2.value == cursor.value
+      c2 = c2.next_node
+    end
+    if c2
+      cursor.next_node = c2.next_node
+    else
+      cursor.next_node = nil
+    end
+  end
+
+  cursor = cursor.next_node
+end
+
+puts "After: #{root}"
