a4c50766c343df3163ab42a4202e7f59f19fa67a
anvox <thien.an.vo.nguyen@gmail.com>
2021-02-06 11:16:13 +0700

    Insert sort linked list

--- linked_list/sort.rb
+++ linked_list/sort.rb
@@ -18,3 +18,31 @@ File.read('./tests/3.txt').split(" ").each do |i|
 end
 
 puts "Before: #{root}"
+
+def sorted_inssert(root, node)
+  if node.value <= root.value
+    node.next_node = root
+    return node
+  end
+
+  curr = root
+  while curr.next_node && curr.next_node.value < node.value
+    curr = curr.next_node
+  end
+  node.next_node = curr.next_node
+  curr.next_node = node
+
+  root
+end
+
+sorted_root = Node.new(root.value)
+curr = root.next_node
+while curr != nil
+  node = Node.new(curr.value)
+
+  sorted_root = sorted_inssert(sorted_root, node)
+
+  curr = curr.next_node
+end
+
+puts "After: #{sorted_root}"
