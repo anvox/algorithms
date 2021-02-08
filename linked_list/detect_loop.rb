e75f720253f257604b52abc020579a402181cb27
anvox <thien.an.vo.nguyen@gmail.com>
2021-02-08 14:21:12 +0700

    Centralize node class

--- linked_list/detect_loop.rb
+++ linked_list/detect_loop.rb
@@ -1,12 +1,4 @@
-class Node
-  def initialize(value, next_node = nil)
-    @value = value
-    @next_node = next_node
-  end
-
-  attr_reader :value
-  attr_accessor :next_node
-end
+require './node'
 
 root = nil
 curr = nil
--- linked_list/find_middle.rb
+++ linked_list/find_middle.rb
@@ -1,22 +1,6 @@
 # require 'pry-byebug'
 
-class Node
-  def initialize(value, next_node = nil)
-    @value = value
-    @next_node = next_node
-  end
-
-  attr_reader :value
-  attr_accessor :next_node
-
-  def to_s
-    if next_node.nil?
-      "(#{value})->NIL"
-    else
-      "(#{value})->#{next_node}"
-    end
-  end
-end
+require './node'
 
 root = nil
 curr = nil
