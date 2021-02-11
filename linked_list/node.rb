57a933fa5512285814d12c725d83374fd97390d3
anvox <thien.an.vo.nguyen@gmail.com>
2021-02-11 09:57:21 +0700

    Flatten

--- linked_list/node.rb
+++ linked_list/node.rb
@@ -15,3 +15,22 @@ class Node
     end
   end
 end
+
+class CNode
+  def initialize(value, next_node = nil, children = nil)
+    @value = value
+    @next_node = next_node
+    @children = children
+  end
+
+  attr_reader :value
+  attr_accessor :next_node, :children
+
+  def to_s
+    if next_node.nil?
+      "(#{value})->NIL"
+    else
+      "(#{value})->#{next_node}"
+    end
+  end
+end
