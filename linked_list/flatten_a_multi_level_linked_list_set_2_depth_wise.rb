a1558583af22fd88cc8747ce732424a12fa7513e
anvox <thien.an.vo.nguyen@gmail.com>
2021-02-20 12:29:34 +0700

    Flatten a multi-level linked list | Set 2 (Depth wise)

---
+++ linked_list/flatten_a_multi_level_linked_list_set_2_depth_wise.rb
@@ -0,0 +1,38 @@
+# Flatten a multi-level linked list | Set 2 (Depth wise)
+
+require './node'
+
+root = CNode.new(10, CNode.new(5, CNode.new(12, CNode.new(7, CNode.new(11),
+                                                          CNode.new(17, CNode.new(6),
+                                                                    CNode.new(9, CNode.new(8),
+                                                                              CNode.new(19, CNode.new(15))))))),
+                 CNode.new(4, CNode.new(20, CNode.new(13, nil,
+                                                      CNode.new(16, nil,
+                                                                CNode.new(3))),
+                                        CNode.new(2))))
+puts "Before: #{root}"
+
+def process(root)
+  stacks = []
+  cur = root
+  flatten = Node.new("mock")
+  flatten_pointer = flatten
+  while cur != nil
+    if cur.next_node
+      stacks << cur.next_node
+    end
+
+    flatten_pointer.next_node = Node.new(cur.value)
+    flatten_pointer = flatten_pointer.next_node
+    cur = cur.children
+
+    if cur == nil
+      cur = stacks.pop
+    end
+  end
+
+  flatten.next_node
+end
+
+root = process(root)
+puts "After: #{root}"
