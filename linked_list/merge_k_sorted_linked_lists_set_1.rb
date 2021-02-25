6334b7d91d86d9e0a6e0a06201befd264d753ebb
anvox <thien.an.vo.nguyen@gmail.com>
2021-02-25 14:28:52 +0700

    Merge K sorted linked lists - HeapSort

--- linked_list/merge_k_sorted_linked_lists_set_1.rb
+++ linked_list/merge_k_sorted_linked_lists_set_1.rb
@@ -59,13 +59,84 @@ def merge_sort(linked_lists)
   linked_lists[0]
 end
 
+# Heap sort
+def create_heap(heap)
+  n = heap.size
+  mid = n / 2 + 1
+  while mid >= 0
+    heapify(heap, mid, n)
+
+    mid -= 1
+  end
+
+  heap
+end
+
+def heapify(heap, i, n)
+  left = i * 2 + 1
+  right = i * 2 + 2
+  min = i
+  if left < n && heap[left].value < heap[min].value
+    min = left
+  end
+  if right < n && heap[right].value < heap[min].value
+    min = right
+  end
+
+  if min != i
+    t = heap[min]
+    heap[min] = heap[i]
+    heap[i] = t
+
+    heap = heapify(heap, min, n)
+  end
+
+  heap
+end
+
 def heap_sort(linked_lists)
-  nil
+  heap = []
+  linked_lists.each do |root|
+    heap << root
+  end
+
+  heap = create_heap(heap)
+
+  root = Node.new('root')
+  cur = root
+  while true
+    if heap[0].next_node == nil
+      # TODO: Optimize, could stop when
+      #   heap[0].next_node == nil AND
+      #   heap[1] presents
+      #   heap[2] not present
+      #   -----
+      #   Just cur -> heap[0] -> heap[1]
+      if heap.size > 1
+        heap[0] = heap.pop
+
+        heap = heapify(heap, 0, heap.size - 1)
+      else
+        cur.next_node = heap[0]
+        break
+      end
+    else
+      t = heap[0]
+      heap[0] = heap[0].next_node
+      heap = heapify(heap, 0, heap.size - 1)
+
+      t.next_node = nil
+      cur.next_node = t
+      cur = t
+    end
+  end
+
+  root.next_node
 end
 
 # Main
 def process(linked_lists)
-  merge_sort(linked_lists)
+  heap_sort(linked_lists)
 end
 
 puts "Before:"
