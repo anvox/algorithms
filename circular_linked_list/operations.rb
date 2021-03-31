e465cc90acb6d87fa90caaa4e8fa780d4cdb42e2
anvox <thien.an.vo.nguyen@gmail.com>
2021-03-31 16:49:28 +0700

    Implement insert_at/remove_at

--- circular_linked_list/operations.rb
+++ circular_linked_list/operations.rb
@@ -3,17 +3,37 @@
 require './node'
 
 def init_circular_linked_list
-  CircularLinkedList.new([1,2,3,4,5,6,7,8])
+  CircularLinkedList.new([0, 1,2,3,4,5,6,7,8])
 end
 
-list = init_circular_linked_list
-puts "Before : #{list}"
+def test_pop_shift
+  list = init_circular_linked_list
+  puts "Before : #{list}"
 
-rev = true
-while value = rev ? list.pop : list.shift
-  puts "#{rev ? "popped" : "shifted"} : #{value}"
-  puts "L : #{list}"
+  rev = true
+  while value = rev ? list.pop : list.shift
+    puts "#{rev ? "popped" : "shifted"} : #{value}"
+    puts "L : #{list}"
 
-  rev = !rev
+    rev = !rev
+  end
 end
 
+def test_index(at_zero = false)
+  list = init_circular_linked_list
+  puts "Before : #{list}"
+  l = list.length
+
+  at = at_zero ? 0 : Random.rand(l + 1)
+  puts "At #{at}: #{list.at(at).value}"
+
+  v = Random.rand(1001)
+  list.insert_at(at, v)
+  puts "Inserted (#{v}) at [#{at}]: #{list}"
+
+  node = list.remove_at(at)
+  puts "Removed (#{node.value}) at [#{at}]: #{list}"
+end
+
+test_index
+test_index(true)
