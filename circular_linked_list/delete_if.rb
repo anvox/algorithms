835d11d817ad4bdab64e59484804657d3401a17a
anvox <thien.an.vo.nguyen@gmail.com>
2021-04-05 17:37:18 +0700

    Add test for delete_if

--- circular_linked_list/delete_if.rb
+++ circular_linked_list/delete_if.rb
@@ -1,14 +1,37 @@
 # Walk through circle linked list and delete nodes satisfy block
-
+#
+# Remove all the Even Digit Sum Nodes from a Circular Singly Linked List
+# Remove all even parity nodes from a Doubly and Circular Singly Linked List
+#
 require './node'
 
 def init_circular_linked_list
-  CircularLinkedList.new([4,5,6,7,8])
+  CircularLinkedList.new([9, 11, 34, 6, 13, 21 ])
+end
+
+def is_even_digit_sum?(num)
+  rem = 0
+  while num > 0
+    rem += num % 10
+    num /= 10
+  end
+
+  rem % 2 == 0
+end
+
+def is_even_parity?(num)
+  rem = 0
+  while num > 0
+    rem += num % 2
+    num /= 2
+  end
+
+  rem % 2 == 0
 end
 
 def process(list)
   list.delete_if do |node|
-    node.value % 2 == 0
+    is_even_parity?(node.value)
   end
 end
 
